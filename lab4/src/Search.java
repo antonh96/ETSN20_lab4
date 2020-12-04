024a95fd2dbe3603ed879a2374cd5eb9cfe76433
antonh96 <antonh96@hotmail.se>
2020-12-04 10:48:16 +0100

    Changed in program

--- lab4/src/Search.java
+++ lab4/src/Search.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class Search {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)  {
         Search search = new Search();
-        search.searchPattern(args[0], args[1]);
+        try {
+            search.searchPattern(args[0], args[1]);
+        } catch (FileNotFoundException e) {
+            System.out.println("File couldn't be found!");
+        }
     }
 
     private void searchPattern(String pattern, String filepath) throws FileNotFoundException {
         Scanner input = new Scanner(new File(filepath));
-        int i=1;
         while (input.hasNext()) {
             String line = input.nextLine();
             if(line.contains(pattern)) {
-                System.out.println("The pattern was found at line: " + i);
+                System.out.println("The pattern was found at line: " + line);
             }
-            i++;
         }
+        input.close();
     }
 
 }
